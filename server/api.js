e4781bc1be4c2131da91378c30d2b78297869d34
Shima <shimaadnanbasmenj@gmail.com>
2023-06-19 12:16:21 +0100

    Login post request

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const express = require("express");
 const app = express();
 const cors = require('cors')
+app.use(express.json())
 app.use(cors())
 const db = new Pool({
     user: 'codeyourfuture',        // replace with you username
@@ -22,6 +23,24 @@ app.get("/jobs", (req, res) => {
 			res.status(500).send(err);
 		});
 });
+
+app.post("/login",(req,res)=>{
+	const sql="select * from login where useremail= ? and password=?";
+	
+	db.query(sql,[req.body.email,req.body.password],(err,data)=>{
+		if(err) return res.json("Error")
+		if(data.length>0){
+			return res.json("Login Successfully")
+		}else{
+			return res.json("no record")
+		}
+	})
+})
+
+
+
+
+
 app.listen(4500, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
 });
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,22 +1,61 @@
 import React ,{useState,useEffect} from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
 export default function Admin() {
-  const [pages, setPages] = useState([]);
+  const[loading,setLoading]=useState(true);
+  const [jobs, setJobs] = useState([]);
+  const[value,setValue]=useState(0);
  
   async function fetchPagesAPI() {
-    const page = await fetch("http://localhost:4500/pages");
+    const page = await fetch("http://localhost:4500/jobs");
     const response = await page.json();
-    setPages(response);
+    setJobs(response);
+    setLoading(false)
 
   }
-
   useEffect(() => {
      fetchPagesAPI();
   }, [])
-   console.log(pages);
+  if(loading){
+    return(
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+  const{company,dates,duties,title}=jobs[value]
   return (
     <div>
-       
+        <section className='section'>
+      <div className='title'>
+        <h1>Experience</h1>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+           {jobs.map((job,index)=>{
+
+            return <button key={job.id} onClick={()=>setValue(index)} className={`job-btn ${value===index} && acive-btn`}>
+              {job.company}</button>
+           })}
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {
+            duties.map((duty,index)=>{
+              return (
+                <div key={index} className='job-desc'>
+                   <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                   <p>{duty}</p>
+                </div>
+              )
+            })
+          }
+        </article>
+      </div>
+    </section>
 
     </div>
   )
